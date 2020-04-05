c4ac1d746201bd08cab3167467953fc3132c465f
Cristiano Pacheco <chris.spb25@gmail.com>
2020-04-04 22:06:17 -0300

    PRIMEIRO RESOURCE - CATEGORY - Resource Categories PUT

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
     public function index()
     {
         return Category::all();
@@ -15,11 +20,7 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'is_active' => 'boolean'
-        ]);
-
+        $this->validate($request, $this->rules);
         return Category::create($request->all());
     }
 
@@ -30,7 +31,9 @@ class CategoryController extends Controller
 
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
     }
 
     public function destroy(Category $category)
