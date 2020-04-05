db2639ee249e556854ff17b5de3ef481a1e56713
Cristiano Pacheco <chris.spb25@gmail.com>
2020-04-05 12:08:53 -0300

    Added incrementing false to models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,5 @@ class Category extends Model
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string'];
+    public $incrementing = false;
 }
