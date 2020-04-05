6a33b42453c8e2e99bd9e2420931a6b1e126b011
Cristiano Pacheco <chris.spb25@gmail.com>
2020-04-05 19:56:01 -0300

    INICIANDO COM TESTES AUTOMATIZADOS NO LARAVEL - Boas práticas com testes unitários

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -3,23 +3,43 @@
 namespace Tests\Unit;
 
 use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testFillableAttribute()
     {
+        $this->assertEquals(['name', 'description', 'is_active'], (new Category())->getFillable());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [SoftDeletes::class, Uuid::class];
+
+        $categoryTraits = array_keys(class_uses(Category::class));
+
+        $this->assertEquals($traits, $categoryTraits);
+    }
+
+    public function testCastsAttribute()
+    {
+        $this->assertEquals(['id' => 'string'], (new Category())->getCasts());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
         $category = new Category();
-        $this->assertEquals(
-            ['name', 'description', 'is_active'],
-            $category->getFillable()
-        );
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+        $this->assertCount(count($dates), $category->getDates());
+    }
+
+    public function testIncrementing()
+    {
+        $this->assertFalse((new Category())->incrementing);
     }
 }
